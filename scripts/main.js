1b6b666412af04d66aa7800aa697348eb0968ab7
adams52 <adams.b52@gmail.com>
2018-06-04 12:22:30 -0500

    Changed the source sheet

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@
 $(function() {
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '1j0HZi8kT4vyWP1hac71ZYwxH5TvSCExQ4sv5uDljJmw' );
+    WTF.init( '13E0a1tarQHw-Dz1q5bWFeKp94HUf_PerolM-6SBaCYQ' );
 
 });
